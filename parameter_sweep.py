f11bbb79de1cd2c5ea1b9ebf3b6fe6b6c7ac00d7
Andres Quan <andresquan1@gmail.com>
2022-04-26 16:03:57 -0600

    Add more parameters to the parameter sweep, use semaphore locks to control writing output

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -2,26 +2,44 @@ import sys
 import glob
 import subprocess
 import multiprocessing
+import logging
+
+lock = multiprocessing.Lock()
 
 
-
-def run_experiment(seed, agents):
-    command = "python3 lca.py --seed " + str(seed) + " --agents " + str(agents)
+def run_experiment(seed, agents, node_degree, radius):
+    command = "python3 lca.py --seed " + str(seed) + " --agents " + str(agents) + " --node_degree " + str(node_degree) + " --radius " + str(radius) 
     command_array = command.split(" ")
     print(command)
-    output = subprocess.check_output(command_array)
+    output = subprocess.check_output(command_array, encoding='UTF-8')
+    print(type(output))
     print(output)
 
+    lock.aquire()
+    logging.info(output)
+    lock.release()
+
 def main():
+
+    
+    fd = open("./results.txt", "w")
     
     #Create tasks
     tasks = []
-    for i in range(0,10):
-        for b in range(50,100):
-            tasks.append((i,b))
-    p = multiprocessing.Pool(4)
+    for i in range(0,3):
+        for b in range(50,100,2):
+            for c in range(2,15):
+                for d in range(10,50,2):
+                    tasks.append((i,b,c,d))
 
+    print("Total of " + str(len(tasks)) + " experiments to be run")
+
+
+    #Create multiprocessing pool
+    p = multiprocessing.Pool(4)
     p.starmap(run_experiment, tasks)
+    p.join()
+    fd.close()
 
 if __name__ == "__main__":
     main()
