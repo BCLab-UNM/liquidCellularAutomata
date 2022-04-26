c11023206b4fc8e53f340509df85e51df55a7a5b
Andres Quan <andresquan1@gmail.com>
2022-04-26 10:56:00 -0600

    Remove old project code from parameter_sweep script

--- parameter_sweep.py
+++ parameter_sweep.py
@@ -26,27 +26,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-"""
-print("CREATING TEST SPECTROGRAMS")
-#Convert all .wav files to spectrogram png files
-for file in filenames:
-    file_no_ext = file.split("/")[-1].replace(".wav","")
-    command = "python3 spectrogram.py " + file + " " + "./data/png_files_test/" + file_no_ext + ".png" 
-    command_array = command.split(" ")
-    print(command)
-    subprocess.check_output(command_array)
-    #subprocess.Popen(command_array, shell=False, stdin=None, stdout=None, stderr=None)
-
-
-#TRAINING DATA
-filenames = glob.glob("./data/wav_files_train/*.wav")
-
-print("CREATING TRAIN SPECTROGRAMS")
-#Convert all .wav files to spectrogram png files
-for file in filenames:
-    file_no_ext = file.split("/")[-1].replace(".wav","")
-    command = "python3 spectrogram.py " + file + " " + "./data/png_files_train/" + file_no_ext + ".png" 
-    print(command)
-    subprocess.check_output(command.split(" "))
-
-"""
