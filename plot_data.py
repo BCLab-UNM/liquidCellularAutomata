cafbf1c7090da32e4ddda8bb0a2ae3199ff0e474
Andres Quan <andresquan1@gmail.com>
2022-04-27 14:49:29 -0600

    Seed will color points in 3d scatterplot

--- plot_data.py
+++ plot_data.py
@@ -3,5 +3,5 @@ import plotly.express as px
 
 df = pd.read_csv('./results-lock.csv')
 
-fig = px.scatter_3d(df, x = 'agents', y = 'iterations', z = 'radius', title='Agents Iterations and Radius')
+fig = px.scatter_3d(df, x = 'agents', y = 'iterations', z = 'radius', color = 'seed', title='Agents Iterations and Radius')
 fig.show()
